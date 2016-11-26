7c545964cf153ed1d4d853a25901e3d759968dc3
Cesar Ortega Ibañez <CesarOrtega@MacBook-Pro-de-Cesar.local>
2016-11-25 20:04:58 -0600

    x

--- wp-config.php
+++ wp-config.php
@@ -23,7 +23,7 @@
 define('DB_NAME', 'directoriomexico_com_mx');
 
 /** MySQL database username */
-define('DB_USER', 'root');
+define('DB_USER', 'ortegon000');
 
 /** MySQL database password */
 define('DB_PASSWORD', '');
